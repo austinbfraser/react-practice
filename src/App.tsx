ac6f873a6fc63262270946ef077724ffa7941a1f
Austin Fraser <austinbfraser@gmail.com>
2025-08-08 16:57:18 -0700

    finished requirements

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,37 @@ import { css } from '@emotion/css';
 import { fetchLastLocation } from './backend/fetchLastLocations';
 
 // This is an example results data structure
-const results: any = [
-  {
-    timestamp: Date.now(),
-    address: {
-      street: '5th Ave',
-      city: 'Random City',
-    },
-    executionTime: 900,
-  },
-  {
-    timestamp: Date.now() + 2000,
-    address: {
-      street: 'Main Road',
-      city: 'New Town',
-    },
-    executionTime: 400,
-  },
-];
+// const results: any = [
+//   {
+//     timestamp: Date.now(),
+//     address: {
+//       street: '5th Ave',
+//       city: 'Random City',
+//     },
+//     executionTime: 900,
+//   },
+//   {
+//     timestamp: Date.now() + 2000,
+//     address: {
+//       street: 'Main Road',
+//       city: 'New Town',
+//     },
+//     executionTime: 400,
+//   },
+// ];
+
+interface MockApiResponse {
+  address: {
+    street: string;
+    city: string;
+    id: string;
+  };
+}
+
+interface Response extends MockApiResponse {
+  timestamp: number;
+  executionTime: number;
+}
 
 const getStyles = () => ({
   button: css`
@@ -49,15 +62,31 @@ const getStyles = () => ({
 });
 
 function App() {
-  const [response, setResponse] = useState(results);
+  const [response, setResponse] = useState<Response[]>([]);
+  let latest;
+  if (response.length > 0) {
+    latest = response[response.length - 1];
+  }
 
   const handleOnClick = async () => {
     const timestamp = Date.now();
 
     await fetchLastLocation().then((res) => {
-      setResponse({ timestamp, ...res });
+      const end = Date.now();
+      const newResponse = [...response];
+      newResponse.push({ timestamp, executionTime: end - timestamp, ...res });
+      setResponse(newResponse);
     });
   };
+
+  let times = response.map((item) => item.executionTime);
+  times.sort((a, b) => a - b);
+  const fastest = times[0];
+  const slowest = times[times.length - 1];
+  const average = Number(
+    (times.reduce((acc, curr) => acc + curr, 0) / times.length).toFixed(0)
+  );
+  
 
   const s = getStyles();
   return (
@@ -76,17 +105,17 @@ function App() {
         </thead>
         <tbody>
           <tr>
-            <td>{response.timestamp}</td>
-            <td>{response?.address?.street}</td>
-            <td>{response?.address?.city}</td>
-            <td></td>
+            <td>{latest?.timestamp}</td>
+            <td>{latest?.address?.street}</td>
+            <td>{latest?.address?.city}</td>
+            <td>{latest?.executionTime}</td>
           </tr>
         </tbody>
       </table>
       <div className={s.statsContainer}>
-        <div>Fastest: ms </div>
-        <div>Slowest: ms </div>
-        <div>Average: ms </div>
+        {typeof fastest === 'number' && <div>Fastest: {fastest} ms </div>}
+        {typeof slowest === 'number' && <div>Slowest: {slowest} ms </div>}
+        {!!average && <div>Average: {average} ms </div>}
       </div>
     </div>
   );
