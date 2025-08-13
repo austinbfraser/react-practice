8eaa1316a1c1dece7af02c3b4d94a06a9401dd5e
Austin Fraser <austinbfraser@gmail.com>
2025-08-13 15:10:36 -0700

    styling for product description

--- src/components/ProductDescription.tsx
+++ src/components/ProductDescription.tsx
@@ -1,38 +1,49 @@
 import { useState } from 'react';
 import CartSVG from './CartSVG';
+import MinusSVG from './MinusSVG';
+import PlusSVG from './PlusSVG';
 
 const ProductDescription = () => {
   const [addToCartNum, setAddToCartNum] = useState<number>(0);
 
   const handleClickMinus = () => {
-    if (addToCartNum > 0) setAddToCartNum(prev => prev - 1)
-  }
+    if (addToCartNum > 0) setAddToCartNum((prev) => prev - 1);
+  };
 
   const handleClickPlus = () => {
-    setAddToCartNum(prev => prev + 1)
-  }
+    setAddToCartNum((prev) => prev + 1);
+  };
 
   return (
     <div className="productDescriptionContainer">
       <div className="productDescriptionContainerInner">
         <div className="sneakerCompany">SNEAKER COMPANY</div>
         <div className="fallLimited">Fall Limited Edition Sneakers</div>
-        <div>
+        <div className='description'>
           These low-profile sneakers are your perfect casual wear companion.
           Featuring a durable outer sole, they'll withstand everything the
           weather can offer.
         </div>
-        <div>$125.00</div>
-        <div>$250.00</div>
+        <div className='price'>
+          $125.00
+          <div className='fiftyPercent'>50%</div>
+          </div>
+        <div className='priceOrig'><s>$250.00</s></div>
         <div className="buttonContainer">
           <div className="addToCartNumContainer">
-            <button className="minus" onClick={handleClickMinus}>-</button>
+            <button className="minus" onClick={handleClickMinus}>
+              <MinusSVG />
+            </button>
             <div className="addToCartNum">{addToCartNum}</div>
-            <button className="plus" onClick={handleClickPlus}>+</button>
+            <button className="plus" onClick={handleClickPlus}>
+              <PlusSVG />
+            </button>
           </div>
-          <div className='addToCartContainer'>
-            <CartSVG />
-            <button className='addToCart'>Add to cart</button>
+          <div className="addToCartContainer">
+            <button className="addToCart">
+              <CartSVG />
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
