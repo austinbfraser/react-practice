1e804862e023357feae9d824d7d0f246cc4ab9a8
Austin Fraser <austinbfraser@gmail.com>
2025-08-12 22:14:13 -0700

    partial progress on product description

--- src/components/ProductDescription.tsx
+++ src/components/ProductDescription.tsx
@@ -3,8 +3,8 @@
 const ProductDescription = () => {
   return (
   <div className="productDescriptionContainer">
-    <div>SNEAKER COMPANY</div>
-    <div>Fall Limited Edition Sneakers</div>
+    <div className="sneakerCompany">SNEAKER COMPANY</div>
+    <div className="fallLimited">Fall Limited Edition Sneakers</div>
     <div>These low-profile sneakers are your perfect casual wear companion.  Featuring a durable outer sole, they'll withstand everything the weather can offer.</div>
     <div>$125.00</div>
     <div>$250.00</div>
