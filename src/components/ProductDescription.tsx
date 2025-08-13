286fa76cfa9c7154969b0c7be55eb95eb9011b2b
Austin Fraser <austinbfraser@gmail.com>
2025-08-13 09:38:20 -0700

    add to cart number buttons working

--- src/components/ProductDescription.tsx
+++ src/components/ProductDescription.tsx
@@ -1,15 +1,43 @@
-// import React from 'react';
+import { useState } from 'react';
+import CartSVG from './CartSVG';
 
 const ProductDescription = () => {
+  const [addToCartNum, setAddToCartNum] = useState<number>(0);
+
+  const handleClickMinus = () => {
+    if (addToCartNum > 0) setAddToCartNum(prev => prev - 1)
+  }
+
+  const handleClickPlus = () => {
+    setAddToCartNum(prev => prev + 1)
+  }
+
   return (
-  <div className="productDescriptionContainer">
-    <div className="sneakerCompany">SNEAKER COMPANY</div>
-    <div className="fallLimited">Fall Limited Edition Sneakers</div>
-    <div>These low-profile sneakers are your perfect casual wear companion.  Featuring a durable outer sole, they'll withstand everything the weather can offer.</div>
-    <div>$125.00</div>
-    <div>$250.00</div>
-  </div>
-    );
+    <div className="productDescriptionContainer">
+      <div className="productDescriptionContainerInner">
+        <div className="sneakerCompany">SNEAKER COMPANY</div>
+        <div className="fallLimited">Fall Limited Edition Sneakers</div>
+        <div>
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable outer sole, they'll withstand everything the
+          weather can offer.
+        </div>
+        <div>$125.00</div>
+        <div>$250.00</div>
+        <div className="buttonContainer">
+          <div className="addToCartNumContainer">
+            <button className="minus" onClick={handleClickMinus}>-</button>
+            <div className="addToCartNum">{addToCartNum}</div>
+            <button className="plus" onClick={handleClickPlus}>+</button>
+          </div>
+          <div className='addToCartContainer'>
+            <CartSVG />
+            <button className='addToCart'>Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDescription;
