ae7b74b1b0524456fb2f191fd1ad41450f8e4149
Austin Fraser <austinbfraser@gmail.com>
2025-08-12 22:02:26 -0700

    scaffolding product description component

--- src/components/ProductDescription.tsx
+++ src/components/ProductDescription.tsx
@@ -1,7 +1,15 @@
 // import React from 'react';
 
 const ProductDescription = () => {
-  return <div className="productDescriptionContainer">ProductDescription</div>;
+  return (
+  <div className="productDescriptionContainer">
+    <div>SNEAKER COMPANY</div>
+    <div>Fall Limited Edition Sneakers</div>
+    <div>These low-profile sneakers are your perfect casual wear companion.  Featuring a durable outer sole, they'll withstand everything the weather can offer.</div>
+    <div>$125.00</div>
+    <div>$250.00</div>
+  </div>
+    );
 };
 
 export default ProductDescription;
